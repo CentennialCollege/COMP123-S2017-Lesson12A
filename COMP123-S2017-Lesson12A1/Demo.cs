6e5b2c80d3f2215c783b364cf626f6328ea5045a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-08-01 15:29:17 -0400

    Created Demo Application

--- COMP123-S2017-Lesson12A1/Demo.cs
+++ COMP123-S2017-Lesson12A1/Demo.cs
@@ -8,11 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * Name: Tom Tsilipoulos
+ * Date: August 1, 2017
+ * Description: This is a Demo Application to showcase User Interface Controls
+ * Version: 0.1 - Created the Project
+ */
+
 namespace COMP123_S2017_Lesson12A1
 {
-    public partial class Form1 : Form
+    public partial class Demo : Form
     {
-        public Form1()
+        public Demo()
         {
             InitializeComponent();
         }
--- COMP123-S2017-Lesson12A1/Program.cs
+++ COMP123-S2017-Lesson12A1/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * Name: Tom Tsilipoulos
+ * Date: August 1, 2017
+ * Description: This is a Demo Application to showcase User Interface Controls
+ * Version: 0.1 - Created the Project
+ */
+
 namespace COMP123_S2017_Lesson12A1
 {
     static class Program
@@ -16,7 +23,7 @@ namespace COMP123_S2017_Lesson12A1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Demo());
         }
     }
 }
